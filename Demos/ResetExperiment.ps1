2d88b7aaa57062fe9751569c3c910ba8cf394c66
unknown <andrewpruski@APAVAILABILITY1.COM>
2019-12-08 21:39:38 +0000

    updated demos

---
+++ Demos/ResetExperiment.ps1
@@ -0,0 +1,20 @@
+
+$Service1 = Get-Service -ComputerName AP-SQLAG-01 -Name MSSQLSERVER
+if($Service1.Status -eq "Stopped"){
+    Get-Service -ComputerName AP-SQLAG-01 -Name MSSQLSERVER | Start-Service
+}
+
+$Service2 = Get-Service -ComputerName AP-SQLAG-02 -Name MSSQLSERVER
+if($Service2.Status -eq "Stopped"){
+    Get-Service -ComputerName AP-SQLAG-02 -Name MSSQLSERVER | Start-Service
+}
+
+$Service3 = Get-Service -ComputerName AP-SQLAG-01 -Name SQLSERVERAGENT
+if($Service3.Status -eq "Stopped"){
+    Get-Service -ComputerName AP-SQLAG-01 -Name SQLSERVERAGENT | Start-Service
+}
+
+$Service4 = Get-Service -ComputerName AP-SQLAG-02 -Name SQLSERVERAGENT
+if($Service4.Status -eq "Stopped"){
+    Get-Service -ComputerName AP-SQLAG-02 -Name SQLSERVERAGENT | Start-Service
+}
