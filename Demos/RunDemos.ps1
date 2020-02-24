4ac52c758faed7676dc0a968b046b9243c685691
unknown <andrewpruski@apavailability1.com>
2020-02-24 19:02:40 +0000

    updated demos

---
+++ Demos/RunDemos.ps1
@@ -0,0 +1,50 @@
+
+
+# Navigate to script location
+Set-Location C:\git\SqlServerChaosEngineering\Demos\AvailabilityGroup
+
+
+
+# Run chaos engineering experiment
+.\AvailabilityGroupExperiment.ps1 -Server AP-SQLAG-01 -AvailabilityGroup AP-SQLAG-03
+
+
+
+# Reset experiment
+.\ResetExperiment.ps1
+
+
+
+# Run experiment one more time
+.\AvailabilityGroupExperiment.ps1 -Server AP-SQLAG-02 -AvailabilityGroup AP-SQLAG-03
+
+
+
+# Reset experiment
+.\ResetExperiment.ps1
+
+
+
+# Retry experiment
+.\AvailabilityGroupExperiment.ps1 -Server AP-SQLAG-02 -AvailabilityGroup AP-SQLAG-03
+
+
+
+# Reset experiment
+.\ResetExperiment.ps1
+
+
+
+# Navigate to Chaos Toolkit experiment
+Set-Location C:\git\SqlServerChaosEngineering\Demos\ChaosToolkit
+
+
+
+# Set environment variables
+$env:SERVER = "AP-SQLAG-01"
+$env:LISTENER_NAME = "AP-SQLAG-03"
+
+
+
+# run experiment
+chaos run experiment.json
