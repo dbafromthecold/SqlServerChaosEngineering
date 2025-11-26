10cc0da5ccd7f16510d768ba2770eae3afd247f3
unknown <andrewpruski@dbafromthecold.com>
2025-11-26 07:32:31 +0000

    updated demos

--- Demos/AvailabilityGroup/ResetExperiment.ps1
+++ Demos/AvailabilityGroup/ResetExperiment.ps1
@@ -1,20 +1,20 @@
 
-$Service1 = Get-Service -ComputerName CHAOS-SQL-01 -Name MSSQLSERVER
+$Service1 = Get-Service -ComputerName AP-SQL2025-01 -Name MSSQLSERVER
 if($Service1.Status -eq "Stopped"){
-    Get-Service -ComputerName CHAOS-SQL-01 -Name MSSQLSERVER | Start-Service
+    Get-Service -ComputerName AP-SQL2025-01 -Name MSSQLSERVER | Start-Service
 }
 
-$Service2 = Get-Service -ComputerName CHAOS-SQL-02 -Name MSSQLSERVER
+$Service2 = Get-Service -ComputerName AP-SQL2025-02 -Name MSSQLSERVER
 if($Service2.Status -eq "Stopped"){
-    Get-Service -ComputerName CHAOS-SQL-02 -Name MSSQLSERVER | Start-Service
+    Get-Service -ComputerName AP-SQL2025-02 -Name MSSQLSERVER | Start-Service
 }
 
-$Service3 = Get-Service -ComputerName CHAOS-SQL-01 -Name SQLSERVERAGENT
+$Service3 = Get-Service -ComputerName AP-SQL2025-01 -Name SQLSERVERAGENT
 if($Service3.Status -eq "Stopped"){
-    Get-Service -ComputerName CHAOS-SQL-01 -Name SQLSERVERAGENT | Start-Service
+    Get-Service -ComputerName AP-SQL2025-01 -Name SQLSERVERAGENT | Start-Service
 }
 
-$Service4 = Get-Service -ComputerName CHAOS-SQL-02 -Name SQLSERVERAGENT
+$Service4 = Get-Service -ComputerName AP-SQL2025-02 -Name SQLSERVERAGENT
 if($Service4.Status -eq "Stopped"){
-    Get-Service -ComputerName CHAOS-SQL-02 -Name SQLSERVERAGENT | Start-Service
+    Get-Service -ComputerName AP-SQL2025-02 -Name SQLSERVERAGENT | Start-Service
 }
