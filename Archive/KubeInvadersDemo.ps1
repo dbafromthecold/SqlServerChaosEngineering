af7bafb788e302cd29e5ede20a2766c298249316
Andrew Pruski <dbafromthecold@gmail.com>
2025-11-27 11:22:14 +0000

    updated kubeinvaders demo

--- Archive/KubeInvadersDemo.ps1
+++ Archive/KubeInvadersDemo.ps1
@@ -20,12 +20,63 @@ cd C:\git\SqlServerChaosEngineering\Demos\KubeInvaders
 
 
 # switch context to cluster
-kubectl config use-context kubeinvaders
+kubectl config use-context kubeinvadersclu1
 
 
 
 # confirm connection to local cluster
 kubectl get nodes
+
+
+
+# deploy one pod running SQL Server and expose
+kubectl apply -f sqlserver-local.yaml
+
+
+
+# view deployment, pod, and service
+kubectl get deployment
+kubectl get pods
+kubectl get service
+
+
+
+# confirm SQL Server version
+$IpAddress=$(kubectl get service sqlserver --no-headers -o custom-columns=":status.loadBalancer.ingress[*].ip")
+mssql-cli -S $IpAddress -U sa -P Testing1122 -Q "SELECT @@VERSION AS [Version];"
+
+
+
+
+# view pod IP address
+kubectl get pods -o wide
+
+
+
+# delete pod
+$PODNAME=$(kubectl get pods --no-headers -o custom-columns=":metadata.name")
+kubectl delete pod $PODNAME
+
+
+
+# view new pod
+kubectl get pods -o wide
+
+
+
+# view service
+kubectl get service
+
+
+
+# run query again
+mssql-cli -S $IpAddress -U sa -P Testing1122 -Q "SELECT @@VERSION"
+
+
+
+# clean up locally
+kubectl delete deployment sqlserver
+kubectl delete service sqlserver
 
 
 
